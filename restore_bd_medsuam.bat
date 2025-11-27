9123c69eb060c72e78409f1472fee38deba57fe5
Ronivaldo Domingues de Andrade <ronidomingues.ard@gmail.com>
2025-11-27 16:57:29 -0300

    doc(BACKUP e RESTORE): Instruções de backup e de restore do database.

---
+++ restore_bd_medsuam.bat
@@ -0,0 +1,12 @@
+# Para BACKUP do banco de dados bd_medsuam, use o seguinte comando:
+# mysqldump -u root -p bd_medsuam > backup_bd_medsuam.sql
+#
+# Para RESTORE do banco de dados bd_medsuam a partir do arquivo de backup, use o seguinte comando:
+# GRANTA antes que o banco de dados bd_medsuam já exista no MySQL.
+# mysql.exe -u root -p bd_medsuam < script_supremo_definitivo_funcional.sql
+
+# Em ambos os casos precione Enter após digitar o comando para inserir a senha do usuário root do MySQL e Enter novamente
+# para executar o comando, pois não há senha.
+
+# EXECUTE ESSE COMANDO NO CMD E NÃO NO POWER SHELL.
+# GARANTA QUE AS VARIÁVEIS DE AMBIENTE DO MYSQL ESTEJAM CONFIGURADAS CORRETAMENTE, CASO CONTRÁRIO, O COMANDO PODE NÃO FUNCIONAR.
